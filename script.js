d1eaae5c45110e0610ba8f0b0f50f51ffaa2f9d8
rodriguezgordillosantiagogg-ship-it <rodriguezgordillosantiagogg@gmail.com>
2026-01-19 20:32:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-function iniciarLibro() {
-  const ancho = window.innerWidth < 600
-    ? window.innerWidth * 0.9
-    : 800;
+console.log("JS cargado");
 
-  const alto = window.innerWidth < 600
-    ? window.innerHeight * 0.6
-    : 500;
-
-  if ($("#book").data("turn")) {
-    $("#book").turn("destroy");
-  }
-
+$(document).ready(function () {
   $("#book").turn({
-    width: ancho,
-    height: alto,
+    width: 800,
+    height: 500,
     autoCenter: true
   });
-}
-
-window.addEventListener("load", iniciarLibro);
-window.addEventListener("resize", iniciarLibro);
+});
