d6aab9a8147c893fa5f68076127c1defa1f56005
rodriguezgordillosantiagogg-ship-it <rodriguezgordillosantiagogg@gmail.com>
2026-01-19 21:27:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(window).on('load', function () {
+$(window).on("load", function () {
 
   const book = $("#book");
 
@@ -8,5 +8,12 @@ $(window).on('load', function () {
     autoCenter: true
   });
 
-});
+  $("#next").click(function () {
+    book.turn("next");
+  });
 
+  $("#prev").click(function () {
+    book.turn("previous");
+  });
+
+});
