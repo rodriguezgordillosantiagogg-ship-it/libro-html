fc1f2a262c468aab6ffe36ec84c00cb11d274c1e
rodriguezgordillosantiagogg-ship-it <rodriguezgordillosantiagogg@gmail.com>
2026-01-19 23:14:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-/* ===== AUDIO + DISCO ===== */
 const audio = document.getElementById("audio");
 const activar = document.getElementById("activar");
-const disco = document.querySelector(".disco");
 
 activar.addEventListener("click", () => {
   audio.play().then(() => {
     activar.remove();
-    disco.classList.add("girando");
   }).catch(err => {
     console.error("Audio bloqueado:", err);
   });
 }, { once: true });
 
-/* ===== LIBRO ===== */
 $(window).on("load", function () {
   $("#book").turn({
     width: 320,
     height: 480,
     autoCenter: true
   });
+});
+
 });
