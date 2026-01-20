c6f40698528f39649198664c69244894f333075d
rodriguezgordillosantiagogg-ship-it <rodriguezgordillosantiagogg@gmail.com>
2026-01-19 23:21:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ const audio = document.getElementById("audio");
 const activar = document.getElementById("activar");
 
 activar.addEventListener("click", () => {
-  audio.play()
-    .then(() => {
-      activar.style.display = "none"; // ← CLAVE
-    })
-    .catch(err => {
-      console.error("Audio bloqueado:", err);
-    });
+  audio.play().then(() => {
+    activar.style.display = "none";
+    activar.style.pointerEvents = "none"; // ← CLAVE PARA PASAR PÁGINAS
+  }).catch(err => {
+    console.error("Audio bloqueado:", err);
+  });
 }, { once: true });
 
 $(window).on("load", function () {
+
+  const ancho = Math.min(window.innerWidth * 0.9, 400);
+  const alto  = ancho * 1.4;
+
   $("#book").turn({
-    width: 320,
-    height: 480,
+    width: ancho,
+    height: alto,
     autoCenter: true
   });
+
 });
+
